bb2fec9eb6e23e8968294e0ba746e51e2480104a
randolf <randolf@localhost>
2015-10-09 19:21:42 +0000

    employ applytrafo

--- ayam/src/aycore/pomesht.c
+++ ayam/src/aycore/pomesht.c
@@ -2693,7 +2693,6 @@ ay_pomesht_connect(ay_object *o1, ay_object *o2,
  ay_point *p1, *p2, *q1, *q2;
  double *vas1 = NULL, *vas2 = NULL;
  double *cv = NULL, dist1, dist2;
- double m[16];
  unsigned int np1 = 0, np2 = 0;
  unsigned int i, j;
  unsigned int numtris = 0, maxtris;
@@ -2746,14 +2745,7 @@ ay_pomesht_connect(ay_object *o1, ay_object *o2,
 
   if(AY_ISTRAFO(o1))
     {
-      ay_trafo_creatematrix(o1, m);
-      cv = pm1->controlv;
-      for(i = 0; i < pm1->ncontrols; i++)
-	{
-	  ay_trafo_apply3(cv, m);
-	  cv += stride;
-	}
-      ay_trafo_defaults(o1);
+      (void)ay_pomesht_applytrafo(o1);
     }
 
   isclosed1 = ay_pomesht_hasedge(pm1, pp[0].index, pp[np1-1].index);
@@ -2775,14 +2767,7 @@ ay_pomesht_connect(ay_object *o1, ay_object *o2,
 
   if(AY_ISTRAFO(o2))
     {
-      ay_trafo_creatematrix(o2, m);
-      cv = pm2->controlv;
-      for(i = 0; i < pm2->ncontrols; i++)
-	{
-	  ay_trafo_apply3(cv, m);
-	  cv += stride;
-	}
-      ay_trafo_defaults(o2);
+      (void)ay_pomesht_applytrafo(o2);
     }
 
   isclosed2 = ay_pomesht_hasedge(pm2, qq[0].index, qq[np2-1].index);
