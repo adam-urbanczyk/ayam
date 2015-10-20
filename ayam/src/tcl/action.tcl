3a63c45c9480b7875581f383147561741065e94d
randolf <randolf@localhost>
2015-10-20 16:25:06 +0000

    imp flashing

--- ayam/src/tcl/action.tcl
+++ ayam/src/tcl/action.tcl
@@ -1476,7 +1476,7 @@ proc actionEditP { w } {
 	undo save EditPnt
 	%W mc
 	%W startpepac %x %y
-	%W pepac -start %x %y	
+	%W pepac -start %x %y
     }
 
     bind $w <B1-Motion> {
@@ -1490,10 +1490,15 @@ proc actionEditP { w } {
     }
 
     actionBindRelease $w
-    if { ($ayprefs(FlashPoints) == 1) && ($ayprefs(FixFlashPoints) == 1) } {
-	bind $w <ButtonRelease-1> "+\
+    if { $ayprefs(FlashPoints) == 1 } {
+	if {$ayprefs(FixFlashPoints) == 1 } {
+	    bind $w <ButtonRelease-1> "+\
           %W startpepac %x %y -flash -ignoreold;\
           %W startpepac %x %y -flash -ignoreold"
+	} else {
+	    bind $w <ButtonRelease-1> "+\
+          %W startpepac %x %y -flash -ignoreold;"
+	}
     }
 
     $w setconf -drawh 1
