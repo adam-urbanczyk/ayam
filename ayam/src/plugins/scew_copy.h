7f3d5af2639dde85e307b715a9ceac05755ead0d
randolf <randolf@localhost>
2016-02-20 20:27:14 +0000

    initial revision

---
+++ ayam/src/plugins/scew_copy.h
@@ -0,0 +1,7 @@
+#ifndef __scew_copy_h__
+#define __scew_copy_h__
+
+scew_element *scew_element_copy (scew_element const *element);
+scew_attribute *scew_attribute_copy (scew_attribute const *attribute);
+
+#endif /* __scew_copy_h__ */
