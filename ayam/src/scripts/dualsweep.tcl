4f9f5c7fb15f9f3949df686b44e7e9bca87742ab
randolf <randolf@localhost>
2017-05-24 19:52:22 +0000

    add property GUI

--- ayam/src/scripts/dualsweep.tcl
+++ ayam/src/scripts/dualsweep.tcl
@@ -1,7 +1,23 @@
+# Ayam, save array: DualSweepAttrData
 # dualsweep.tcl: example script for Ayam Script object
 # this script wants Script Object Type "Modify"
+# it also has a property GUI, just add a tag "NP DualSweepAttr"
+# to the Script object to see it
+if { ![info exists ::DualSweepAttrData] } {
+    array set ::DualSweepAttrData {
+	Type 2
+	Sections 0
+	SP {Type Sections}
+    }
+}
+if { ![info exists ::DualSweepAttrGUI] } {
+    set w [addPropertyGUI DualSweepAttr "" ""]
+    addMenu $w DualSweepAttrData Type {Open Closed Periodic}
+    addParam $w DualSweepAttrData Sections
+}
+
 proc crtSweep { x y z } {
-    global transfPropData SweepAttrData
+    global transfPropData SweepAttrData DualSweepAttrData
     copOb
     crtOb Sweep
     goDown -1
@@ -12,28 +28,30 @@ proc crtSweep { x y z } {
     set dy [expr -($transfPropData(Translate_Y)+$y)]
     set dz [expr -($transfPropData(Translate_Z)+$z)]
     movOb $dx $dy $dz
-    applyTrafo -all   
+    applyTrafo -all
     goUp
     selOb -1
     getProp
-    set SweepAttrData(Type) 2
-    #set SweepAttrData(Sections) 30
+    set SweepAttrData(Type) $DualSweepAttrData(Type)
+    set SweepAttrData(Sections) $DualSweepAttrData(Sections)
     #set SweepAttrData(Interpolate) 1
     setProp
     notifyOb
-    convOb -inplace   
+    convOb -inplace
 }
 
 convOb -inplace NCurve
 withOb 0 { getPnt -e -r 0 x0 y0 z0; getPnt -e -r 1 x1 y1 z1; }
 selOb 0 1
-crtSweep $x0 $y0 $z0 
+crtSweep $x0 $y0 $z0
 selOb 0 2
 crtSweep $x1 $y1 $z1
 selOb 3 4
-makecompNP -u -l 1
+makeCompNP -u -l 1
 crtOb NPatch -width 2 -height 2 -cv { 0 0 0 1  1 1 0 1  0 0 1 1  1 1 1 1 }
 selOb 3 4 5
 tweenNP
 selOb 0 1 2 3 4 5
 delOb
+
+# dualsweep.tcl
