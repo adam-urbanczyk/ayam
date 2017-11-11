66519f46ecfe60282e79a82ef481cd8703573ed9
randolf <randolf@localhost>
2017-11-11 10:04:16 +0000

    prepare release 1.25

--- ayam/src/aycore/read.c
+++ ayam/src/aycore/read.c
@@ -183,6 +183,12 @@ ay_read_header(FILE *fileptr)
 
   ay_read_version = 1;
 
+  if(!strcmp(version, "1.25"))
+    {
+      ay_read_version = 16;
+      version_unknown = AY_FALSE;
+    }
+  else
   if(!strcmp(version, "1.24"))
     {
       ay_read_version = 16;
