9bc173bffe2da2ae8b17658428034f359499d667
Randolf Schultz <randolf.schultz@gmail.com>
2018-04-30 18:13:04 +0200

    allow spaces in balloons

--- ayam/src/tcl/view.tcl
+++ ayam/src/tcl/view.tcl
@@ -1267,7 +1267,7 @@ proc viewSetMAIcon { w image balloon } {
 	set conf "$w.$m configure"
 	eval "$conf -image $image"
 	if { $balloon != "" } {
-	    eval [subst "balloon_set $w.$m $balloon"]
+	    eval [subst "balloon_set $w.$m \"$balloon\""]
 	} else {
 	    eval [subst "balloon_clear $w.$m"]
 	}
