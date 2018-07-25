983289642131cc3f8e5c343f1a66a172bfb404d4
Randolf Schultz <randolf.schultz@gmail.com>
2018-07-25 18:44:57 +0200

    add subdivision curves

---
+++ ayam/src/plugins/sdcurve.tcl
@@ -0,0 +1,44 @@
+# Ayam, a free 3D modeler for the RenderMan interface.
+#
+# Ayam is copyrighted 1998-2017 by Randolf Schultz
+# (randolf.schultz@gmail.com) and others.
+#
+# All rights reserved.
+#
+# See the file License for details.
+
+# sdcurve.tcl - Sdcurve objects Tcl code
+
+global ay SDCurve_props SDCurveAttr SDCurveAttrData
+
+set SDCurve_props { Transformations Attributes Tags SDCurveAttr }
+
+array set SDCurveAttr {
+arr   SDCurveAttrData
+sproc ""
+gproc ""
+w     fSDCurveAttr
+}
+
+array set SDCurveAttrData {
+Type 0
+}
+
+# create SDCurveAttr-UI
+set w [frame $ay(pca).$SDCurveAttr(w)]
+addVSpace $w s1 2
+
+addCheck $w SDCurveAttrData Closed
+addParam $w SDCurveAttrData Length
+set l [list Chaikin Cubic]
+addMenu $w SDCurveAttrData Type $l
+addParam $w SDCurveAttrData Level
+
+
+# add menu entry to the "Create/Custom Object" sub-menu
+mmenu_addcustom SDCurve "crtOb SDCurve;uS;sL;rV"
+
+# tell the rest of Ayam (or other custom objects), that we are loaded
+lappend ay(co) SDCurve
+
+# EOF
