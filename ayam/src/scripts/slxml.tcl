327d724a1c00b80c90a6559e3956177a8bbfe741
Randolf Schultz <randolf.schultz@gmail.com>
2018-11-25 11:04:23 +0100

    imp parsing robustness and error reporting

--- ayam/src/scripts/slxml.tcl
+++ ayam/src/scripts/slxml.tcl
@@ -14,22 +14,30 @@ set AYUSESLXARGS 0
 set ay(sext) ".sl"
 #set ayprefs(StripShaderArch) 0
 
+proc shaderScanError { at ln fn } {
+ ayError 2 shaderScanXML [concat "No" $at "attribute in:" $ln "(" $fn ")"]
+}
+
 proc shaderScan { file varname } {
     global ay ayprefs
     upvar 1 $varname shader
 
     append file ".sl"
 
-    set spathstr [split [shader_unglobShaderPaths "$ayprefs(Shaders)"] $ay(separator)]
+    set spathstr [split [shader_unglobShaderPaths "$ayprefs(Shaders)"]\
+		      $ay(separator)]
     set f ""
     foreach p $spathstr {
-	set f [open [file join $p $file]]
-	if { $f != "" } {
-	    break;
+	set s [file join $p $file]
+	if { [file exists $s] } {
+	    catch {set f [open $s]}
+	    if { $f != "" } {
+		break;
+	    }
 	}
     }
     if { $f == "" } {
-	ayError 10 shader_scanXML $file
+	ayError 10 shaderScanXML $file
 	return;
     }
     set shader ""
@@ -42,17 +50,43 @@ proc shaderScan { file varname } {
 	    set done 1
 	} else {
 	    if { [string first "<shader " $rl] > -1 } {
-		regexp -- {^.*<shader type="([^"]*)" name="([^"]*)"} $rl a b c
-		lappend shader $c
-		lappend shader $b
+		regexp -- {.*type="([^"]*)"} $rl a t
+		if {$a != ""} {
+		  regexp -- {.*name="([^"]*)"} $rl a n
+		  if {$a == ""} {
+		      shaderScanError name $rl $file
+		      break;
+		  }
+		} else {
+		    shaderScanError type $rl $file
+		    break;
+		}
+		lappend shader $n
+		lappend shader $t
 	    }
 	    # if is shader
 	    if { [string first "<argument " $rl] > -1 } {
-		regexp -- {^.*<argument name="([^"]*)".*type="([^"]*)".*value="([^"]*)"} $rl a b c d
-#the following comment just helps emacsens font lock mode
-#"
+		regexp -- {.*name="([^"]*)"} $rl a n
+                if {$a != ""} {
+		  regexp -- {.*type="([^"]*)"} $rl a t
+                  if {$a != ""} {
+		    regexp -- {.*value="([^"]*)"} $rl a v
+                    #the following comment just helps emacsens font lock mode
+                    #"
+                    if {$a == ""} {
+			shaderScanError value $rl $file
+			break;
+                    }
+		  } else {
+		      shaderScanError type $rl $file
+		      break;
+		  }
+                } else {
+		    shaderScanError name $rl $file
+		    break;
+		}
 	        set parameter ""
-	        lappend parameter $b $c 0 $d
+	        lappend parameter $n $t 0 $v
 	        lappend parameters $parameter
 	    }
 	    # if is argument
@@ -62,6 +96,8 @@ proc shaderScan { file varname } {
     # while not done
 
     lappend shader $parameters
+
+    catch [close $f]
 
  return;
 }
