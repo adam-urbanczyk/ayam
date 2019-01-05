105326b647cf721ed539beb4aa8357a99d565b69
Randolf Schultz <randolf.schultz@gmail.com>
2019-01-05 17:17:03 +0100

    fix memory access

--- ayam/src/objects/parab.c
+++ ayam/src/objects/parab.c
@@ -1212,7 +1212,7 @@ ay_paraboloid_convertcb(ay_object *o, int in_place)
 	{ free(new->refine); free(new); return ay_status; }
 
       t = new->down;
-      while(t->next)
+      while(t && t->next)
 	{
 	  t = t->next;
 	}
