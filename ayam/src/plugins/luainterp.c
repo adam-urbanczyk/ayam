3b8900526f3ef79e03ebbc0737653867e9ea0d09
Randolf Schultz <randolf.schultz@gmail.com>
2019-11-02 20:37:26 +0100

    add isTrimmed command

--- ayam/src/plugins/luainterp.c
+++ ayam/src/plugins/luainterp.c
@@ -113,6 +113,7 @@ static const struct luaL_Reg luainterplib [] = {
       {"isClosed", luainterp_wraptclcmd},
       {"isPlanar", luainterp_wraptclcmd},
       {"isParent", luainterp_wraptclcmd},
+      {"isTrimmed", luainterp_wraptclcmd},
 
       {"mergePo", luainterp_wraptclcmd},
       {"optiPo", luainterp_wraptclcmd},
