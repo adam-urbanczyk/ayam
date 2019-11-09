d865c6f8b6288035998eb2055a4f6b360e5721de
Randolf Schultz <randolf.schultz@gmail.com>
2019-11-09 10:52:26 +0100

    imp format and UI creation

--- ayam/src/plugins/csphere.tcl
+++ ayam/src/plugins/csphere.tcl
@@ -14,32 +14,35 @@ global ay CSphere_props CSphereAttr CSphereAttrData
 set CSphere_props { Transformations Attributes Material Tags CSphereAttr }
 
 array set CSphereAttr {
-arr   CSphereAttrData
-sproc ""
-gproc ""
-w     fCSphereAttr
-
+    arr   CSphereAttrData
+    sproc ""
+    gproc ""
+    w     fCSphereAttr
 }
 
 array set CSphereAttrData {
-Closed 1
-Radius 1.0
-ZMin -1.0
-ZMax 1.0
-ThetaMax 1.0
+    Closed 1
+    Radius 1.0
+    ZMin -1.0
+    ZMax 1.0
+    ThetaMax 1.0
 }
+
+set ay(bok) $ay(appb)
 
 # create CSphereAttr-UI
 set w [frame $ay(pca).$CSphereAttr(w)]
+set a $CSphereAttr(arr)
 
-addCheck $w CSphereAttrData Closed
-addParam $w CSphereAttrData Radius
-addParam $w CSphereAttrData ZMin
-addParam $w CSphereAttrData ZMax
-addParam $w CSphereAttrData ThetaMax
+addVSpace $w s1 2
+addCheck $w $a Closed
+addParam $w $a Radius
+addParam $w $a ZMin
+addParam $w $a ZMax
+addParam $w $a ThetaMax
 
 # add menu entry to the "Create/Custom Object" sub-menu
-mmenu_addcustom CSphere "crtOb CSphere;uS;sL;rV"
+mmenu_addcustom CSphere "crtOb CSphere;uCR;sL;rV"
 
 # tell the rest of Ayam (or other custom objects), that we are loaded
 lappend ay(co) CSphere
