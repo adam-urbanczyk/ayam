28ba025080738a8effcc9b04a25fabf8aec3922e
Randolf Schultz <randolf.schultz@gmail.com>
2019-11-27 19:38:12 +0100

    imp handling of multiple dropped files

--- ayam/src/plugins/aydnd.tcl
+++ ayam/src/plugins/aydnd.tcl
@@ -22,90 +22,90 @@
 proc aydnd_handlefiledrop { data type } {
     global ay ayprefs ay_error
 
-    set insert 0
-    foreach newfilename $data {
-
-    if { $newfilename != "" } {
+    if { $data != "" } {
 	viewCloseAll
 	cS; plb_update
 	set ay(askedscriptdisable) 0
 	update
-	set filename $newfilename
+    } else {
+	# issue error message?
+	return;
+    }
 
+    set insert 0
+    foreach filename $data {
 	set ay_error ""
 	update
 
 	# see, if this is an Ayam scene file
-	set ext [file extension $filename ]
+	set ext [file extension $filename]
 	if { ($ext != "") && ([string compare -nocase $ext ".ay"]) } {
 	    # no, try to import it
 	    io_importScene $filename
-	    return;
-	}
+	} else {
+	    # this is a Ayam scene
 
-	# make backup copy
-	if { $ayprefs(BakOnReplace) == 1 } {
-	    set err [ catch {
+	    # make backup copy
+	    if { $ayprefs(BakOnReplace) == 1 } {
+		set err [ catch {
 		file copy -force -- $filename ${filename}${ayprefs(BackupExt)}
-	    } ]
+		} ]
+	    }
+
+	    # change working directory
+	    if { [file exists $filename] } {
+		cd [file dirname $filename]
+	    }
+
+	    # read the file
+	    if { $insert } {
+		set fname "insertScene"
+		insertScene $filename
+	    } else {
+		set fname "replaceScene"
+		replaceScene $filename
+	    }
+	    if { $ay_error < 2 } {
+		if { ! $insert } {
+		    set windowfilename [file tail [file rootname $filename]]
+		    wm title . "Ayam - Main - $windowfilename : --"
+		    set ay(filename) $filename
+		    ayError 4 $fname "Done reading scene from: $filename"
+		    # restore main window geometry from tag
+		    io_readMainGeom
+		} else {
+		    ayError 4 $fname "Done inserting scene from: $filename"
+		}
+	    } else {
+		wm title . "Ayam - Main - : --"
+		set ay(filename) ""
+		ayError 2 $fname "Failed reading scene from: $filename"
+	    }
+
+	    goTop
+	    selOb
+	    set ay(CurrentLevel) "root"
+	    set ay(SelectedLevel) "root"
+	    update
+	    uS
+	    rV
+	    # add scene file to most recently used list
+	    io_mruAdd $filename
+	    # the following Ayam scene files will be inserted
+	    set insert 1
 	}
-
-	# change working directory
-	if { [file exists $filename] } {
-	    set dirname [file dirname $filename]
-	    cd $dirname
-	    set .fl.con(-prompt) {[file tail [pwd]]>}
-	    .fl.con delete end-1lines end
-	    Console:prompt .fl.con "\n"
-	}
-
-	# read the file
-	if { $insert } {
-	    set fname "insertScene"
-	    insertScene $filename
-
-	} else {
-	    set fname "replaceScene"
-	    replaceScene $filename
-	}
-	if { $ay_error < 2 } {
-	    set windowfilename [file tail [file rootname $filename]]
-	    wm title . "Ayam - Main - $windowfilename : --"
-	    set ay(filename) $filename
-	    ayError 4 $fname "Done reading scene from:"
-	    ayError 4 $fname "$filename"
-
-	    # restore main window geometry from tag
-	    io_readMainGeom
-
-	} else {
-	    wm title . "Ayam - Main - : --"
-	    set ay(filename) ""
-	    ayError 2 "Ayam" "There were errors while loading:"
-	    ayError 2 "Ayam" "$filename"
-	}
-
-	goTop
-	selOb
-	set ay(CurrentLevel) "root"
-	set ay(SelectedLevel) "root"
-	update
-	uS
-	rV
-	# add scene file to most recently used list
-	io_mruAdd $filename
-	# reset scene change indicator
-	set ay(sc) 0
-	update
-	foreach view $ay(views) { viewBind $view }
-	update
-
-	after idle viewMouseToCurrent
-    }
-    # if
-    set insert 1
+	# if
     }
     # foreach
+
+    # reset scene change indicator
+    set ay(sc) 0
+    update
+    foreach view $ay(views) { viewBind $view }
+    update
+
+    after idle viewMouseToCurrent
+
  return;
 }
 # aydnd_handlefiledrop
@@ -122,7 +122,7 @@ package require tkdnd
 cd $origwd
 
 tkdnd::drop_target register . DND_Files
-bind . <<Drop>> {aydnd_handlefiledrop  %D %T}
+bind . <<Drop>> {aydnd_handlefiledrop %D %T}
 
 # bind views to image files (load as bgimage)
 # bind views to images (load as bgimage)
