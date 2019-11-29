476981d9bdb5abbe708faf38d7968943ba452a13
Randolf Schultz <randolf.schultz@gmail.com>
2019-11-29 19:32:16 +0100

    imp custom component type UI

--- ayam/src/contrib/meta/metacomp.tcl
+++ ayam/src/contrib/meta/metacomp.tcl
@@ -14,6 +14,14 @@ global ay MetaComp_props MetaCompAttr MetaCompAttrData
 
 set MetaComp_props { Transformations Attributes Tags MetaCompAttr }
 
+proc metacomp_setAttr { } {
+    global ay MetaCompAttr MetaCompAttrData
+    set t $ay(pca).$MetaCompAttr(w).tScript
+    set MetaCompAttrData(Expression) [$t get 1.0 end]
+    setProp
+ return;
+}
+
 proc metacomp_getAttr { } {
     global ay MetaCompAttr MetaCompAttrData
 
@@ -22,36 +30,102 @@ proc metacomp_getAttr { } {
     getProp
 
     set ay(bok) $ay(appb)
+    set f $MetaCompAttr(arr)
 
-    addMenu $w MetaCompAttrData Formula {MetaBalls Torus Cube Heart Custom}
-    addCheck $w MetaCompAttrData Negative
+    addMenu $w $f Formula {MetaBalls Torus Cube Heart Custom}
+    addCheck $w $f Negative
 
     if { $MetaCompAttrData(Formula) == 0 } {
-	addParam $w MetaCompAttrData Radius
-	addParam $w MetaCompAttrData EnergyCoeffA
-	addParam $w MetaCompAttrData EnergyCoeffB
-	addParam $w MetaCompAttrData EnergyCoeffC
+	addParam $w $f Radius
+	addParam $w $f EnergyCoeffA
+	addParam $w $f EnergyCoeffB
+	addParam $w $f EnergyCoeffC
     }
-    if { $MetaCompAttrData(Formula) == 1 } {	
-	addParam $w MetaCompAttrData Ri
-	addParam $w MetaCompAttrData Ro
-	addCheck $w MetaCompAttrData Rotate
+    if { $MetaCompAttrData(Formula) == 1 } {
+	addParam $w $f Ri
+	addParam $w $f Ro
+	addCheck $w $f Rotate
     }
     if { $MetaCompAttrData(Formula) == 2 } {
-	addParam $w MetaCompAttrData EdgeX
-	addParam $w MetaCompAttrData EdgeY
-	addParam $w MetaCompAttrData EdgeZ
+	addParam $w $f EdgeX
+	addParam $w $f EdgeY
+	addParam $w $f EdgeZ
     }
     if { $MetaCompAttrData(Formula) == 3 } {
 
     }
 
     if { $MetaCompAttrData(Formula) == 4 } {
-	addString $w MetaCompAttrData Expression {
-	    { set f [expr {pow($x,4)+pow($y,4)+pow($z,4)}] }
-	    { set f [expr {sqrt(pow($y,2)+pow($z,2))}] }
-	    { set r 1.0;set f [expr {sqrt(pow($y,2)+pow($z,2))}];if {$x < -$r} {set f [expr {sqrt(pow(($x+$r),2)+pow($y,2)+pow($z,2))}]}; if {$x > $r} {set f [expr {sqrt(pow(($x-$r),2)+pow($y,2)+pow($z,2))}]} } }
+	addText $w t1 "Expression:"
+
+	pack [text $w.tScript -undo 1 -width 40 -height 6]
+	set t $w.tScript
+	if { $ay(ws) == "Aqua" } {
+	    $t conf -relief sunken -bd 2
+	}
+	eval [subst "bindtags $t \{$t Text all\}"]
+	bind $t <Key-Escape> "resetFocus;break"
+
+	# create resize handle for text widget
+	if { $ay(ws) == "Win32" } {
+	    resizeHandle:Create $w.rsh $t -bg SystemWindow
+	} else {
+	    resizeHandle:Create $w.rsh $t
+	}
+
+	$t delete 1.0 end
+	$t insert 1.0 $MetaCompAttrData(Expression)
+	if { $MetaCompAttrData(Expression) != "" } {
+	    $t delete "end - 1 chars"
+	}
+	# create popup menu
+	set m [menu $t.popup -tearoff 0]
+	$m add command -label "Clear All" -command "$t delete 1.0 end"
+	$m add command -label "Paste (Replace)" -command "_pasteToText $t"
+
+	$m add command -label "Load from file" -command {
+	    global ay ScriptAttr
+	    set types {{"Script Files" {".tcl" ".js" ".lua"}} {"All files" *}}
+	    set newfilename ""
+	    set newfilename [tk_getOpenFile -filetypes $types -parent .\
+				 -title "Select file to load:"]
+	    if { $newfilename != "" } {
+		set scfile ""
+		set scfile [open $newfilename r]
+		if { $scfile != "" } {
+		    set nt [read $scfile]
+		    if { $nt != "" } {
+			$ay(pca).${ScriptAttr(w)}.tScript delete 1.0 end
+			$ay(pca).${ScriptAttr(w)}.tScript insert end $nt
+		    }
+		}
+	    }
+	}
+	$m add separator
+	set i 0
+	foreach expression $MetaCompAttrData(Expressions) {
+	    $m add command -label [lindex $expression 0] -command \
+	      "$t delete 1.0 end; $t insert end \{[lindex $expression 1]\}"
+
+	    incr i
+	}
+
+	# bind popup menu
+	set mb 3
+	if { $ay(ws) == "Aqua" && $ayprefs(SwapMB) } {
+	    set mb 2
+	}
+	bind $t <$mb> {
+	    global ay MetaCompAttr
+	    set ay(xy) [winfo pointerxy .]
+	    tk_popup $ay(pca).${MetaCompAttr(w)}.tScript.popup\
+		[lindex $ay(xy) 0] [lindex $ay(xy) 1]
+	}
+	# bind
+
+	after 100 "resizeHandle:PlaceHandle $w.rsh $t"
     }
+    # if custom
 
     $ay(pca) itemconfigure 1 -window $w
 
@@ -68,16 +142,20 @@ proc metacomp_getAttr { } {
 
 
 array set MetaCompAttr {
-arr   MetaCompAttrData
-sproc ""
-gproc metacomp_getAttr
-w     fMetaCompAttr
-
+    arr   MetaCompAttrData
+    sproc metacomp_setAttr
+    gproc metacomp_getAttr
+    w     fMetaCompAttr
 }
 
 array set MetaCompAttrData {
     Formula  0
+    Expressions {
+	    { RoundCube "expr {pow($x,4)+pow($y,4)+pow($z,4)}" }
+	    { OpenCylinder "expr {sqrt(pow($y,2)+pow($z,2))}" }
+    }
 }
+# { set r 1.0;set f [expr {sqrt(pow($y,2)+pow($z,2))}];if {$x < -$r} {set f [expr {sqrt(pow(($x+$r),2)+pow($y,2)+pow($z,2))}]}; if {$x > $r} {set f [expr {sqrt(pow(($x-$r),2)+pow($y,2)+pow($z,2))}]} } }
 
 # add menu entry to Create/Custom sub-menu
 mmenu_addcustom MetaComp "crtOb MetaComp;uS;sL;rV"
