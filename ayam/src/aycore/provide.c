98d2121e04dac196342014cfeda6f144e04fb9dc
Randolf Schultz <randolf.schultz@gmail.com>
2020-11-18 16:36:15 +0100

    add peek functionality

--- ayam/src/aycore/provide.c
+++ ayam/src/aycore/provide.c
@@ -97,6 +97,8 @@ ay_provide_object(ay_object *o, unsigned int type, ay_object **result)
 
 /** ay_provide_nptoolobj:
  * helper for tool objects that provide NURBS surfaces with caps/bevels
+ * Also supports peek-mode, in which case the caps and bevels will be
+ * omitted.
  *
  * \param[in] o original tool object
  * \param[in] type desired target object type
@@ -120,13 +122,31 @@ ay_provide_nptoolobj(ay_object *o, unsigned int type,
   if(!result)
     {
       if(type == AY_IDNPATCH)
-	return AY_OK;
+	{
+	  return AY_OK;
+	}
       else
-	return AY_ERROR;
+	{
+	  if(type == UINT_MAX-AY_IDNPATCH)
+	    {
+	      return AY_OK;
+	    }
+	  else
+	    {
+	      return AY_ERROR;
+	    }
+	}
     }
 
   if(type != AY_IDNPATCH)
-    return AY_OK;
+    {
+      if(type == UINT_MAX-AY_IDNPATCH)
+	{
+	  /* peek */
+	  *result = npatch;
+	}
+      return AY_OK;
+    }
 
   if(!npatch)
     return AY_ERROR;
@@ -177,3 +197,67 @@ ay_provide_nptoolobj(ay_object *o, unsigned int type,
 
  return ay_status;
 } /* ay_provide_nptoolobj */
+
+
+/** ay_provide_peek:
+ * call provide callback of an object in peek mode
+ * The result will be a reference to the internal data structure of the
+ * inquired object. There is no need to free the returned data but it
+ * should also be considered immutable.
+ *
+ * \param[in] o object to process
+ * \param[in] type desired type of object to be provided by \a o (AY_ID...)
+ * \param[in,out] result where to store the resulting reference,
+ *   may be NULL to designate a check
+ *
+ * \returns AY_OK on success, error code otherwise.
+ */
+int
+ay_provide_peek(ay_object *o, unsigned int type, ay_object **result)
+{
+ int ay_status = AY_OK;
+ char fname[] = "peek";
+ ay_voidfp *arr = NULL;
+ ay_providecb *cb = NULL;
+
+  if(!o)
+    return AY_ENULL;
+
+  /* call the provide callback */
+  arr = ay_providecbt.arr;
+  cb = (ay_providecb *)(arr[o->type]);
+  if(cb)
+    {
+      ay_status = cb(o, UINT_MAX-type, result);
+
+      if(!result)
+	{
+	  /* caller just wants to test ability to provide
+	     => return ay_status without checking */
+	  return ay_status;
+	}
+
+      if(ay_status)
+	{
+	  ay_error(AY_ERROR, fname, "provide callback failed");
+	  return AY_ERROR;
+	}
+    }
+  else
+    {
+      if(!result)
+	{
+	  /* caller just wants to test ability to provide
+	     and this object has no provide callback at all... */
+	  return AY_ERROR;
+	}
+        /*
+	  else
+	  {
+	  ay_error(AY_ERROR, fname, "No provide callback registered!");
+	  }
+	*/
+    } /* if */
+
+ return AY_OK;
+} /* ay_provide_peek */
