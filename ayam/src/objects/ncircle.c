60e30686a057f19900be6873eceeed35a683dccf
Randolf Schultz <randolf.schultz@gmail.com>
2020-11-22 08:59:45 +0100

    add peek

--- ayam/src/objects/ncircle.c
+++ ayam/src/objects/ncircle.c
@@ -699,7 +699,7 @@ ay_ncircle_providecb(ay_object *o, unsigned int type, ay_object **result)
 
   if(!result)
     {
-      if(type == AY_IDNCURVE)
+      if((type == AY_IDNCURVE) || (type == UINT_MAX-AY_IDNCURVE))
 	return AY_OK;
       else
 	return AY_ERROR;
@@ -709,6 +709,15 @@ ay_ncircle_providecb(ay_object *o, unsigned int type, ay_object **result)
 
   if(!ncircle)
     return AY_ENULL;
+
+  if(type == UINT_MAX-AY_IDNCURVE)
+    {
+      /* peek */
+      if(!ncircle->ncurve)
+	return AY_ERROR;
+      *result = ncircle->ncurve;
+      return AY_OK;
+    }
 
   if(type == AY_IDNCURVE)
     {
